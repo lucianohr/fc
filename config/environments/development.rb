1bc2acdb69483fa4fcf293fd208f0f2c55bd1788
Luciano H. Ribeiro <hr.luciano@gmail.com>
2014-06-17 22:45:45 -0300

    Ajustes em devise. Trello card #7
    https://trello.com/c/eJrSVkcv

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,16 @@ Fc::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'hr.luciano@gmail.com',
+    password:             'fyiytbsqetbigmav', # https://security.google.com/settings/security/apppasswords?pli=1
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
