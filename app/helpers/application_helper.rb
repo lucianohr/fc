f3deda52c5b32902675eac616835977fc7655ccf
Luciano H. Ribeiro <hr.luciano@gmail.com>
2015-06-06 22:33:07 -0300

    Corrigido menu de autenticação.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, page_title.to_s
   end
+
+  def alert_class_for(flash_type)
+    {
+        :success => 'alert-success',
+        :error => 'alert-danger',
+        :alert => 'alert-warning',
+        :notice => 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 end
